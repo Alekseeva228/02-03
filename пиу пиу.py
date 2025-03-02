3cd2ee88109363817836f491f9ee40fbe2d85b2d
Alekseeva228 <tumonya79@gmail.com>
2025-03-02 17:25:29 +0700

    Add files via upload

---
+++ пиу пиу.py
@@ -0,0 +1,10 @@
+vl = int(input("Введите число: "))
+temp = int(input("Введите число:"))
+
+if  vl >= 60 and temp <= 10:
+    print("Пойдет")
+    
+if  vl <= 30 and temp >= 30:
+    print("Нормик")
+    
+print("тревога!")
