d72473eeb169f1d6a4cbf8ce13d3f5790c1c4522
Alekseeva228 <tumonya79@gmail.com>
2025-03-02 16:58:40 +0700

    Add files via upload

---
+++ уоуо.py
@@ -0,0 +1,21 @@
+tsvet = input("Введите Пароль: ")
+
+if  tsvet == "Super puper" or tsvet == "929":
+    print("Отлчино!")
+    print("Успешный вход")
+    
+
+if  not tsvet == "Super puper" or tsvet == "929":
+    print("Ошибка!")
+    print("Логин неверен")
+    
+
+if  tsvet == "Super puper" or tsvet == "929":
+    print("Ошибка!")
+    print("Пароль неверен")
+    
+
+if  not tsvet == "Super puper" or not tsvet == "929":
+    print("Ошибка!")
+    print("Логин и пароль неверный")
+    
